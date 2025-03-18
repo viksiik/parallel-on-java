0167168a13c522ef8047332229dbc110f447258b
viksiik <viksiik.k@gmail.com>
2025-03-18 09:02:25 +0200

    added lab 3_1exp

--- lab3/src/main/java/Main.java
+++ lab3/src/main/java/Main.java
@@ -13,29 +13,32 @@ public class Main {
     }
 
     public static void main(String[] args) {
-        int size = 1500;
-        System.out.println("Generating matrices...");
-        int[][] A = generateMatrix(size);
-        int[][] B = generateMatrix(size);
-        System.out.println("Matrices generated.");
+        int[] sizes = {200, 500, 1000, 1500, 2000};
+        for (int size : sizes) {
+            System.out.println("\nMatrix size: " + size + "x" + size);
+            System.out.println("Generating matrices...");
+            int[][] A = generateMatrix(size);
+            int[][] B = generateMatrix(size);
+            System.out.println("Matrices generated.");
 
-        long startTime, endTime;
+            long startTime, endTime;
 
-        System.out.println("Sequential multiplication:");
-        startTime = System.nanoTime();
-        Result resultSeq = MatrixMultiplier.multiplySequential(A, B);
-        endTime = System.nanoTime();
-        double timeSeq = (endTime - startTime) / 1e6;
-        System.out.println("Time: " + timeSeq + " ms\n");
+            System.out.println("Sequential multiplication:");
+            startTime = System.nanoTime();
+            Result resultSeq = MatrixMultiplier.multiplySequential(A, B);
+            endTime = System.nanoTime();
+            double timeSeq = (endTime - startTime) / 1e6;
+            System.out.println("Time: " + timeSeq + " ms");
 
-        System.out.println("Parallel multiplication:");
-        startTime = System.nanoTime();
-        Result resultParallel = MatrixMultiplier.multiplyParallel(A, B);
-        endTime = System.nanoTime();
-        double timeParallel = (endTime - startTime) / 1e6;
-        System.out.println("Time: " + timeParallel + " ms");
+            System.out.println("Parallel multiplication:");
+            startTime = System.nanoTime();
+            Result resultParallel = MatrixMultiplier.multiplyParallel(A, B);
+            endTime = System.nanoTime();
+            double timeParallel = (endTime - startTime) / 1e6;
+            System.out.println("Time: " + timeParallel + " ms");
 
-        double speedup = timeSeq / timeParallel;
-        System.out.println("Speedup: " + String.format("%.2f", speedup));
+            double speedup = timeSeq / timeParallel;
+            System.out.println("Speedup: " + String.format("%.2f", speedup));
+        }
     }
 }
