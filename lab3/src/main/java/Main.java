003a6049c547c057715da6148c971b2f8fc05f70
viksiik <viksiik.k@gmail.com>
2025-03-18 13:30:05 +0200

    added lab 3_2exp

--- lab3/src/main/java/Main.java
+++ lab3/src/main/java/Main.java
@@ -13,29 +13,31 @@ public class Main {
     }
 
     public static void main(String[] args) {
-        int size = 1500;
+        int size = 1000;
+        int blockSize = 100;
+        int[] threadCounts = {1, 2, 4, 8, 16};
+
         System.out.println("Generating matrices...");
         int[][] A = generateMatrix(size);
         int[][] B = generateMatrix(size);
         System.out.println("Matrices generated.");
 
-        long startTime, endTime;
-
-        System.out.println("Sequential multiplication:");
-        startTime = System.nanoTime();
-        Result resultSeq = MatrixMultiplier.multiplySequential(A, B);
-        endTime = System.nanoTime();
+        System.out.println("Sequential Fox multiplication:");
+        long startTime = System.nanoTime();
+        int[][] resultSeq = FoxMatrixExperiment.foxSequential(A, B, blockSize);
+        long endTime = System.nanoTime();
         double timeSeq = (endTime - startTime) / 1e6;
         System.out.println("Time: " + timeSeq + " ms\n");
 
-        System.out.println("Parallel multiplication:");
-        startTime = System.nanoTime();
-        Result resultParallel = MatrixMultiplier.multiplyParallel(A, B);
-        endTime = System.nanoTime();
-        double timeParallel = (endTime - startTime) / 1e6;
-        System.out.println("Time: " + timeParallel + " ms");
-
-        double speedup = timeSeq / timeParallel;
-        System.out.println("Speedup: " + String.format("%.2f", speedup));
+        for (int threads : threadCounts) {
+            System.out.println("Parallel Fox multiplication with " + threads + " threads:");
+            startTime = System.nanoTime();
+            int[][] resultParallel = FoxMatrixExperiment.foxParallel(A, B, blockSize, threads);
+            endTime = System.nanoTime();
+            double timeParallel = (endTime - startTime) / 1e6;
+            double speedup = timeSeq / timeParallel;
+            System.out.println("Time: " + timeParallel + " ms");
+            System.out.println("Speedup: " + String.format("%.2f", speedup) + "\n");
+        }
     }
 }
--- lab3/src/main/java/MatrixMultiplier.java
+++ lab3/src/main/java/MatrixMultiplier.java
@@ -1,27 +1,41 @@
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
+import java.util.Random;
 
-class MatrixMultiplier {
-    public static Result multiplyParallel(int[][] A, int[][] B) {
-        int rowsA = A.length;
-        int colsA = A[0].length;
-        int colsB = B[0].length;
+class FoxMatrixExperiment {
+    public static int[][] foxSequential(int[][] A, int[][] B, int blockSize) {
+        int n = A.length;
+        int[][] C = new int[n][n];
 
-        Result result = new Result(rowsA, colsB);
-        ExecutorService executor = Executors.newFixedThreadPool(Runtime.getRuntime().availableProcessors());
-
-        for (int i = 0; i < rowsA; i++) {
-            final int row = i;
-            executor.execute(() -> {
-                int[] rowData = new int[colsB];
-                for (int j = 0; j < colsB; j++) {
-                    for (int k = 0; k < colsA; k++) {
-                        rowData[j] += A[row][k] * B[k][j];
+        for (int stage = 0; stage < n / blockSize; stage++) {
+            for (int i = 0; i < n; i += blockSize) {
+                for (int j = 0; j < n; j += blockSize) {
+                    for (int k = 0; k < n; k += blockSize) {
+                        multiplyBlock(A, B, C, i, j, k, blockSize);
                     }
                 }
-                result.setRow(row, rowData);
-            });
+            }
+        }
+        return C;
+    }
+
+    public static int[][] foxParallel(int[][] A, int[][] B, int blockSize, int numThreads) {
+        int n = A.length;
+        int[][] C = new int[n][n];
+        ExecutorService executor = Executors.newFixedThreadPool(numThreads);
+
+        for (int stage = 0; stage < n / blockSize; stage++) {
+            for (int i = 0; i < n; i += blockSize) {
+                for (int j = 0; j < n; j += blockSize) {
+                    final int row = i, col = j;
+                    executor.execute(() -> {
+                        for (int k = 0; k < n; k += blockSize) {
+                            multiplyBlock(A, B, C, row, col, k, blockSize);
+                        }
+                    });
+                }
+            }
         }
 
         executor.shutdown();
@@ -31,26 +45,16 @@ class MatrixMultiplier {
             e.printStackTrace();
         }
 
-        return result;
+        return C;
     }
 
-    public static Result multiplySequential(int[][] A, int[][] B) {
-        int rowsA = A.length;
-        int colsA = A[0].length;
-        int colsB = B[0].length;
-
-        Result result = new Result(rowsA, colsB);
-
-        for (int i = 0; i < rowsA; i++) {
-            int[] rowData = new int[colsB];
-            for (int j = 0; j < colsB; j++) {
-                for (int k = 0; k < colsA; k++) {
-                    rowData[j] += A[i][k] * B[k][j];
+    private static void multiplyBlock(int[][] A, int[][] B, int[][] C, int row, int col, int k, int blockSize) {
+        for (int i = row; i < row + blockSize; i++) {
+            for (int j = col; j < col + blockSize; j++) {
+                for (int l = k; l < k + blockSize; l++) {
+                    C[i][j] += A[i][l] * B[l][j];
                 }
             }
-            result.setRow(i, rowData);
         }
-
-        return result;
     }
 }
